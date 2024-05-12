89113d3c22e454b4b3b5eb610c8649804fc50f94
Knight1354 <77206546+Knight1354@users.noreply.github.com>
2024-05-13 00:50:25 +0530

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const musicRoutes = require('./routes/musicRoutes');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use('/public', express.static('assets/music'))
+app.use(bodyParser.json());
+
+// Middleware
+app.use(cors());
+
+// Routes
+app.use('/music', musicRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
